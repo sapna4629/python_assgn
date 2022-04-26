a2e924601d0b2aa43dab527cd77c2da284458826
sapna <sapna.bharanikumar@gmail.com>
2022-04-26 20:33:10 +0530

    assignment 1

---
+++ fm_sub.py
@@ -0,0 +1,11 @@
+#wap to add two numbers
+
+print('code started')
+
+def main():
+	num1=int(input('enter a number:'))
+	num2=int(input('enter another number:'))
+	print(f'{num1}-{num2}={num1-num2}')
+
+main()
+print('code ended')
